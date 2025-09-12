216b586e4582213f0204bb677d2befb64f9f74c1
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2025-09-13 00:48:55 +0530

    Setup neon Postgres w/ Drizzle

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+// The corrected code (correct)
+export default {
+    schema: "./src/models/*.js",
+    out: "./drizzle",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL,
+    },
+};
